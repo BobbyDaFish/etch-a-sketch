a1aae3cf5c8e46e65d28e15a7f8a89b285bc1e29
BobbyDaFish <130507595+BobbyDaFish@users.noreply.github.com>
2024-03-20 14:48:31 -0600

    Create variables for row and cell divs

--- js/script.js
+++ js/script.js
@@ -1,2 +1,7 @@
 let gridSize = 16;
 
+const gridRow = document.createElement('div');
+const gridCell = document.createElement('div');
+
+gridRow.setAttribute('class', 'grid-row');
+gridCell.setAttribute('class', 'grid-cell');
