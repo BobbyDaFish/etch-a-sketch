e3287ccad1b8bfe7a92818e9924d75292e75c574
BobbyDaFish <130507595+BobbyDaFish@users.noreply.github.com>
2024-03-20 14:56:04 -0600

    Create functions for adding rows and cells

--- js/script.js
+++ js/script.js
@@ -1,7 +1,21 @@
 let gridSize = 16;
 
+const boardCont = document.querySelector('#board-cont');
 const gridRow = document.createElement('div');
 const gridCell = document.createElement('div');
 
 gridRow.setAttribute('class', 'grid-row');
 gridCell.setAttribute('class', 'grid-cell');
+
+function createGrid(){
+    for (i = gridSize; i > 0; 1--) {
+        boardCont.appendChild(gridRow).setAttribute('id', i);
+        insertCells(i);
+    }
+}
+
+function insertCells(rowID){
+    for (c = gridSize; c > 0; c--){
+        document.getElementById(rowID).appendChild(gridCell);
+    }
+}
