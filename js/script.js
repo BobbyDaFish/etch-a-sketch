847b8abcc97489cd6d919699157d6795f53daaee
BobbyDaFish <130507595+BobbyDaFish@users.noreply.github.com>
2024-03-21 15:59:58 -0600

    Refactor grid creation to improve speed
    Moved the addition of event listeners to occur after the grid is created

--- js/script.js
+++ js/script.js
@@ -3,36 +3,33 @@ let gridSize = 16;
 const boardCont = document.querySelector('#board-cont');
 const newGridBtn = document.querySelector('#new-board');
 
-const gridRow = document.createElement('div');
-const gridCell = document.createElement('div');
-
-gridRow.setAttribute('class', 'grid-row');
-gridCell.setAttribute('class', 'grid-cell');
-
 function createGrid(){ // add a rows to the grid with counting ids 
     for (i = gridSize; i > 0; i--) {
-        let newRow = gridRow.cloneNode();
-        boardCont.appendChild(newRow).setAttribute('id', i);
+        const gridRow = document.createElement('div');
+        
+        gridRow.setAttribute('class', 'grid-row');
+
+        boardCont.appendChild(gridRow).setAttribute('id', i);
         insertCells(i);
+        const allCells = document.querySelectorAll('.grid-cell');
+          
+        for (const a of allCells) { // add listener to each cell to add class to change color.
+        a.addEventListener('mouseenter', function (e){
+            e.stopImmediatePropagation();
+            this.setAttribute("class", "grid-dark");
+        });
+    }
     }
 }
 
 function insertCells(rowID){ // add cells to each row
     for (c = gridSize; c > 0; c--){
-        let newCell = gridCell.cloneNode();
-        document.getElementById(rowID).appendChild(newCell);
+        const gridCell = document.createElement('div');
+        gridCell.setAttribute('class', 'grid-cell');
 
-        const allCells = document.querySelectorAll('.grid-cell');
-          
-            for (const a of allCells) { // add listener to each cell to add class to change color.
-            a.addEventListener('mouseenter', function (e){
-                e.stopImmediatePropagation();
-                this.setAttribute("class", "grid-dark");
-            });
-        }
-
-        
+        document.getElementById(rowID).appendChild(gridCell);
     }
+
 }
 
 createGrid();
